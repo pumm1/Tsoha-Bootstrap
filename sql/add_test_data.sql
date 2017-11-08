58df30265787cd107f6f6908d648b87077f1ec87
pumm1 <patsu01@gmail.com>
2017-11-08 19:31:11 +0200

    sql tietokantaa

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,11 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Person (name, password) VALUES ('savolanen', 'qwerty');
+INSERT INTO Person (name, password) VALUES ('martta64', 'salasana');
+
+INSERT INTO Ingredient (name) VALUES ('suklaa');
+INSERT INTO Ingredient (name) VALUES ('nakki');
+
+INSERT INTO Category (name) VALUES ('Ruoka');
+INSERT INTO Category (name) VALUES ('Juoma');
+
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,30 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Person(
+	id SERIAL PRIMARY KEY,
+	name varchar(10) NOT NULL,
+	password varchar(20) NOT NULL
+);
+
+CREATE TABLE Category(
+	id SERIAL PRIMARY KEY,	
+	name varchar(40) NOT NULL
+);
+
+CREATE TABLE Ingredient(
+	id SERIAL PRIMARY KEY,
+	name varchar(40) NOT NULL
+);
+
+CREATE TABLE Recipe(
+	id SERIAL PRIMARY KEY,
+	category_id INTEGER REFERENCES Category(id),
+	person_id INTEGER REFERENCES Person(id),
+	name varchar(40) NOT NULL,
+	info varchar(500) NOT NULL
+);
+
+CREATE TABLE Addtable(
+	recipe_id INTEGER REFERENCES Recipe(id),
+	ingredient_id INTEGER REFERENCES Ingredient(id)
+);
+
