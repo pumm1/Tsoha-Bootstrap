16d6c7f5a4dfe3ff5c37134ed77ed9dbd024d206
pumm1 <patsu01@gmail.com>
2017-11-23 11:06:04 +0200

    checkboxit kategorioille

--- app/models/Recipe.php
+++ app/models/Recipe.php
@@ -8,13 +8,21 @@ class Recipe extends BaseModel {
         parent::__construct($attributes);
         $this->validators = array('validateName', 'validateInfo');
     }
-    
-    public function validateName(){
+
+    public function validateName() {
         return $this->validate_string($this->name, 3, 'name');
     }
-    
-    public function validateInfo(){
+
+    public function validateInfo() {
         return $this->validate_string($this->info, 10, 'info');
+    }
+    
+    public function validatePerson(){
+        return $this->validate_string($this->person_id, 1, 'person_id');
+    }
+
+    public function validateCategory() {
+        return $this->validateString($this->category_id, 1, 'category_id');
     }
 
     public function save() { //ei staattinen!
@@ -26,8 +34,6 @@ class Recipe extends BaseModel {
         $row = $query->fetch();
         $this->id = $row['id'];
     }
-
-
 
     public static function findLatestId() {
 
